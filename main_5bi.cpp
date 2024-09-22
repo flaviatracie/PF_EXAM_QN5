8b5b43ae4cd64324732db928377c04bdd304df39
flaviatracie <flaviatracie256@gmail.com>
2024-09-22 12:50:26 +0200

    Add files via upload

---
+++ main_5bi.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Print out my name
+    cout << "My name is Anena Flavia Tracy." << endl;
+    return 0;
+}
---
+++ main_5bii.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Print multiples of 3 from 1 to 20
+    for (int i = 1; i <= 20; i++) {
+        if (i % 3 == 0) {
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
